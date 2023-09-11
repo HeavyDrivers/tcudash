3056399c81062555123a99a1125e38d01b28719e
i-ujjwaljha <findingaman25@gmail.com>
2023-09-11 18:56:20 +0530

    new commit

---
+++ src/routes.jsx
@@ -0,0 +1,73 @@
+import {
+  HomeIcon,
+  UserCircleIcon,
+  TableCellsIcon,
+  BellIcon,
+  ArrowRightOnRectangleIcon,
+  UserPlusIcon,
+  MapIcon,
+} from "@heroicons/react/24/solid";
+import { Home, Profile, Tables, Notifications, Maps } from "@/pages/dashboard";
+import { SignIn, SignUp } from "@/pages/auth";
+
+const icon = {
+  className: "w-5 h-5 text-inherit",
+};
+
+export const routes = [
+  {
+    layout: "dashboard",
+    pages: [
+      {
+        icon: <HomeIcon {...icon} />,
+        name: "dashboard",
+        path: "/home",
+        element: <Home />,
+      },
+      {
+        icon: <UserCircleIcon {...icon} />,
+        name: "profile",
+        path: "/profile",
+        element: <Profile />,
+      },
+      {
+        icon: <TableCellsIcon {...icon} />,
+        name: "tables",
+        path: "/tables",
+        element: <Tables />,
+      },
+      {
+        icon: <BellIcon {...icon} />,
+        name: "notifications",
+        path: "/notifications",
+        element: <Notifications />,
+      },
+      {
+        icon: <MapIcon {...icon} />,
+        name: "Fleet Management",
+        path: "/fleet-management",
+        element: <Maps />,
+      },
+    ],
+  },
+  {
+    title: "auth pages",
+    layout: "auth",
+    pages: [
+      {
+        icon: <ArrowRightOnRectangleIcon {...icon} />,
+        name: "sign in",
+        path: "/sign-in",
+        element: <SignIn />,
+      },
+      {
+        icon: <UserPlusIcon {...icon} />,
+        name: "sign up",
+        path: "/sign-up",
+        element: <SignUp />,
+      },
+    ],
+  },
+];
+
+export default routes;
